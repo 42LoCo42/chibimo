46d093f3f026068e718054fd55baa8e2d69e34dc
Leon Schumacher <leonsch@protonmail.com>
2022-05-28 17:45:25 +0200

    Add music library preference

--- app/src/main/java/forty/two/chibimo/SettingsActivity.kt
+++ app/src/main/java/forty/two/chibimo/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package forty.two.chibimo
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts.GetContent
+import androidx.activity.result.contract.ActivityResultContracts.OpenDocumentTree
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity: AppCompatActivity() {
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.settings_activity)
@@ -19,8 +22,17 @@ class SettingsActivity: AppCompatActivity() {
 	}
 
 	class SettingsFragment: PreferenceFragmentCompat() {
+		private val fileSelector = registerForActivityResult(OpenDocumentTree()) {
+			println(it)
+			preferenceManager.sharedPreferences?.edit()?.putString("musicDirectory", it.toString())
+		}
+
 		override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 			setPreferencesFromResource(R.xml.root_preferences, rootKey)
+			findPreference<Preference>("musicDirectory")?.setOnPreferenceClickListener {
+				fileSelector.launch(Uri.EMPTY)
+				true
+			}
 		}
 	}
 }
